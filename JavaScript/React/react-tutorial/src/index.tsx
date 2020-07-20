344780b2d82b600c92ee7fc2f2dd376d9fcc2986
MH4GF <rixi436@gmail.com>
2020-07-20 22:07:14 +0900

    チュートリアル最後まで

--- JavaScript/React/react-tutorial/src/index.tsx
+++ JavaScript/React/react-tutorial/src/index.tsx
@@ -74,6 +74,7 @@ interface IGameState {
   history: {
     squares: (string | null)[];
   }[];
+  stepNumber: number;
   xIsNext: boolean;
 }
 
@@ -86,12 +87,13 @@ class Game extends React.Component<{}, IGameState> {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i: number) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -105,14 +107,31 @@ class Game extends React.Component<{}, IGameState> {
           squares: squares,
         },
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc: string = move ? `Go to move #${move}` : `Go to game start`;
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -132,7 +151,7 @@ class Game extends React.Component<{}, IGameState> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
